9a7e5ded6bbb66f28c9b8764a055db1a13e2031b
rewit <rewit@naver.com>
2019-05-30 16:20:18 +0900

    Jave_02

---
+++ src/Hello_01.java
@@ -0,0 +1,25 @@
+
+public class Hello_01 {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("대한민국만세");
+		System.out.println("0000000000");
+		System.out.println("OOOOOOOOOO");
+		System.out.println("ooooooooooo");
+		System.out.println("IIIIIIII");
+		System.out.println("iiiiiiiii");
+		System.out.println("llllllllll");
+		System.out.println("LLLLLLLLLLL");
+		System.out.println("111111111111");
+		
+		boolean bYes = 10 != 20;
+		bYes = 10 >= 20;
+		
+		bYes = 10 <= 20;
+		bYes = 10 == 20;
+		
+	}
+
+}
